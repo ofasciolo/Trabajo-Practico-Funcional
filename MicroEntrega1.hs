d0ab1f3d46186fc4ce90077271ccdac73f6b6889
tomastm19 <38118995+tomastm19@users.noreply.github.com>
2018-05-05 13:48:09 -0300

    agregue replicate
    agregue la funcion replicate para que sea una memoria vacia porque sino falla el lod al no haber posicion

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -3,7 +3,7 @@ module MicroEntrega1 where
 --3.1
 data Microprocesador = Microprocesador{memoria::[Int] ,acumuladorA::Int, acumuladorB::Int, programCounter::Int,etiqueta::String} deriving Show
 --3.1.a
-xt8088 =  Microprocesador{memoria = [] , acumuladorA = 0, acumuladorB = 0,programCounter = 0, etiqueta = ""}
+xt8088 =  Microprocesador{memoria = replicate 1024 0 , acumuladorA = 0, acumuladorB = 0,programCounter = 0, etiqueta = ""}
 
 
 --3.2.1
@@ -49,7 +49,7 @@ lod addr unProcesador = unProcesador{acumuladorA=memoria unProcesador !! (addr-1
 dividir valor1 valor2 unProcesador = divide(lod 1 (swap(lod 2 (str 2 valor2 (str 1 valor1 unProcesador)))))
 
 --4.2.3
-fp20 = Microprocesador{memoria=[], acumuladorA = 7, acumuladorB = 24, programCounter = 0, etiqueta = ""}
+fp20 = Microprocesador{memoria=replicate 1024 0, acumuladorA = 7, acumuladorB = 24, programCounter = 0, etiqueta = ""}
 
 --4.3.4
 at8086 = Microprocesador {memoria =[1..20], acumuladorA = 0, acumuladorB = 0, programCounter = 0, etiqueta = ""}
