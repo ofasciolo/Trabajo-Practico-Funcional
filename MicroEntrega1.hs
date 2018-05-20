11af05542bc271158bd355720841510980962522
dariokozicki <38699660+dariokozicki@users.noreply.github.com>
2018-05-20 19:01:22 -0300

    nopN

--- MicroEntrega1.hs
+++ MicroEntrega1.hs
@@ -71,10 +71,6 @@ nop = avanzarCounter
 avanzarCounter :: Instruccion
 avanzarCounter unProcesador = unProcesador {programCounter = programCounter unProcesador +1}
 
-nopN :: Int -> Instruccion
-nopN 1 = nop
-nopN n = nop.(nopN (n-1))
-
 lod :: Int->Instruccion
 lod addr unProcesador = unProcesador{acumuladorA=memoria unProcesador !! (addr-1)}  --usamos la funcion !! para acceder al item addr de la lista (-1 para acomodar con lo pedido)
 
